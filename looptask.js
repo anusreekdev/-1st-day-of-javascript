d95e24b4d036291fd086f9bf94b0698cc14be261
Anusree <anusreektinku@gmail.com>
2026-07-23 09:26:44 +0530

    add

---
+++ looptask.js
@@ -0,0 +1,190 @@
+// for (let i=2;i<=100;i+=3 ){
+//     console.log(i);
+    
+// }
+
+// for (let i=5;i<=100;i+=5){
+//     console.log(i);
+    
+// }
+
+// for (let a=7;a<=50;a+=7){
+//     console.log(a);
+    
+// }
+
+// for (let i=1;i<=40;i++){
+//     if(i%4==0){
+//         console.log(i);
+        
+//     }
+// }
+
+// for (let i=7;i<=100;i++){
+//     if(i%7==0){
+//         console.log(i);
+        
+//     }
+// }
+
+// for (let i=50;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i);
+        
+//     }
+// }
+
+// for (let i=51;i<=99;i++){
+//     if(i%2!=0){
+//         console.log(i);
+        
+//     }
+// }
+
+// let sum=0
+// for (let i=1;i<=5;i++){
+//     sum=sum+i
+
+
+// }
+// console.log(sum);
+
+//  let sum=0
+//  for (let i=1;i<=50;i++){
+//     sum=sum+i
+//  }
+//  console.log(sum);
+ 
+// let sum=0
+// for (let i=1;i<=10;i++){
+//   if(i%2==0){
+//     // console.log(i);
+//     sum=sum+i
+//   }
+// }
+// console.log(sum);
+
+// let sum=0
+// for (let i=1;i<=20;i++){
+//     if(i%2!=0){
+// sum=sum+i
+//     }
+// }
+// console.log(sum);
+
+ 
+//  
+
+// for (let i=1;i<=20;i++){
+//     console.log("age:"+i);
+    
+// }
+
+// for (let i=1;i<=20;i++){
+//     console.log(`number is ${i}`);
+    
+// }
+
+//  let num=3
+//  for (let i=1;i<=10;i++){
+//     console.log(`${i} X ${num} = ${i*num}`);
+    
+//  }
+
+// let num=3
+// for (let i=1;i<=10;i++){
+//     console.log("" +i+ "X" +num+ "="+num*i );
+    
+// }
+ 
+
+
+// let fact=1
+// for (let i=1;i<=6;i++){
+//     fact=fact*i
+  
+// }
+//  console.log(fact); 
+    
+// let fact=1
+// for (let i=1;i<=7;i++){
+//     fact=fact*i
+// }
+// console.log(fact);
+
+
+
+// for (let i=1;i<=15;i++){
+//     console.log(i*i);
+// }
+
+
+// let num=5
+// console.log(num*num);
+
+//     let num=5
+// console.log(" cube is  " +(num*num*num) );
+
+
+// for (let i=1;i<=10;i++){
+    
+//     console.log("cube is "+(i*i*i));
+// }
+
+
+// let num=10
+// console.log(`${num}cube is${num*num*num}`);
+
+// for (let i=1;i<=5;i++){
+//     console.log(`${i} cube is ${i*i*i} `);
+    
+// }
+// let sum=0
+// for (let i=1;i<=5;i++){
+//     sum=sum+(i*i)
+
+    
+// }
+
+//     console.log(sum);
+
+
+
+// 1
+// for (let i=1;i<=50;i++){
+//     if(i%5==0){
+//         console.log(i);
+        
+//     }
+// }
+
+// for (let i=1;i<=50;i++){
+//     if(i%10==0){
+//         console.log(i);
+        
+//     }
+// }
+
+// 2
+
+// let sum=0
+// for (let i=1;i<=5;i++){
+//     sum=sum+(i*i*i)
+// }
+// console.log(sum);
+
+// 3
+
+// for (let i=1;i<=100;i++){
+//     if(i%2==0){
+//         console.log(i);
+        
+//     }
+// }
+
+// for (let i=1;i<=100;i++){
+//     if(i%3==0){
+//         console.log(i);
+        
+//     }
+// }
